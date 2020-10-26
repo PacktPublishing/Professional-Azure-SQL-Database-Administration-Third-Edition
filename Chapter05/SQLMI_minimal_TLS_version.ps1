2100bf881256a3c5a7f9343c7be2c83eb9e5489c
Shashikant Shakya <shashikantshakya@gmail.com>
2020-10-27 00:55:28 +0530

    Add files via upload

---
+++ Chapter05/SQLMI_minimal_TLS_version.ps1
@@ -0,0 +1,17 @@
+﻿
+#setting up variable as per our environment
+$MisubId = "6ee856b5-yy6d-4bc1-xxxx-byg5569842e1"
+$InstanceName = "packtsqlmi"
+$ResourceGroup = "Packt"
+
+#Login to Azure Account
+Connect-AzAccount
+
+# Use your subscription ID in place of subscription-id below
+Select-AzSubscription -SubscriptionId $MisubId
+
+#Get the Minimal TLS Version property
+(Get-AzSqlInstance -Name $InstanceName -ResourceGroupName $ResourceGroup).MinimalTlsVersion
+
+# Update Minimal TLS Version Property
+Set-AzSqlInstance -Name $InstanceName -ResourceGroupName $ResourceGroup -MinimalTlsVersion "1.1"
