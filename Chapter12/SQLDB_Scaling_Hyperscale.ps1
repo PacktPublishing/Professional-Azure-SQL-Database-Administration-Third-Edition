3f31e04c6844c1ca0832b3a473a26e35ebd460dc
Shashikant Shakya <shashikantshakya@gmail.com>
2020-12-31 11:09:20 +0530

    Add files via upload

---
+++ Chapter12/SQLDB_Scaling_Hyperscale.ps1
@@ -0,0 +1,14 @@
+﻿#setting up variable as per your environment
+$subscription = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx"
+$resourceGroup = "SQLServer"
+$serverName = "toyfactory1"
+$databaseName = "toystore1"
+$edition = "Hyperscale"
+$sku = "HS_Gen5_2"
+$replicaCount =1
+
+#Select the Azure SQL Database subscription
+Select-AzSubscription -SubscriptionId $subscription
+
+#Updating existing Azure SQL Database to Hyperscale service tier.
+Set-AzSqlDatabase -ResourceGroupName $resourceGroup -DatabaseName $databaseName -ServerName $serverName -Edition $edition -RequestedServiceObjectiveName $sku -ReadReplicaCount $replicaCount
